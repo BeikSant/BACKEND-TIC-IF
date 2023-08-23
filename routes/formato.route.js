34130d1739acc5ae2326075040db552156f0840e
BeikSant <beiker.santorum@unl.edu.ec>
2023-08-22 23:03:41 -0500

    test de integracion del formato del informe final

--- routes/formato.route.js
+++ routes/formato.route.js
@@ -7,12 +7,10 @@ const formatoRouter = express.Router();
 formatoRouter.get('/', requireToken,isDirector, formatoController.obtenerTodos);
 formatoRouter.post('/', requireToken, isDirector, formatoController.crear);
 formatoRouter.patch('/:id', requireToken, isDirector, formatoController.actualizar)
-
 formatoRouter.get('/defecto', requireToken, isDirector, formatoController.obtenerPorDefecto)
 formatoRouter.patch('/estado/:id', requireToken, isDirector, formatoController.cambiarEstado);
 formatoRouter.get('/active', requireToken, formatoController.obtenerActivo)
 formatoRouter.get('/:formato', requireToken, formatoController.obtenerUno)
-
 formatoRouter.delete('/:id', requireToken, isDirector, formatoController.eliminar)
 
 export default formatoRouter
